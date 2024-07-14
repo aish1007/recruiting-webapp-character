bf98c530566c92bd9d1ab0ca3377eb4f2196bea6
Aishwarya Narayanan <aishwarya.narayanan025@gmail.com>
2024-07-14 12:22:49 -0700

    Add controls to create characters

--- src/App.js
+++ src/App.js
@@ -1,10 +1,36 @@
-import { useState } from 'react';
-import './App.css';
-import { ATTRIBUTE_LIST, CLASS_LIST, SKILL_LIST } from './consts.js';
-
+import { useState } from "react";
+import "./App.css";
+import { ATTRIBUTE_LIST, SKILL_LIST } from "./consts.js";
 
 function App() {
-  const [num, setNum] = useState(0);
+  const [characters, setCharacters] = useState([]);
+
+  const getSkills = () => {
+    const skillList = {};
+    SKILL_LIST.forEach((skill) => (skillList[skill.name] = 0));
+    return skillList;
+  };
+
+  const getAttributes = () => {
+    const attributesList = {};
+    ATTRIBUTE_LIST.forEach((attribute) => (attributesList[attribute] = 10));
+    return attributesList;
+  };
+
+  const addCharacter = () => {
+    const newCharacter = {
+      id: Date.now(),
+      name: `Character ${characters.length + 1}`,
+      attributes: getAttributes(),
+      skills: getSkills(),
+    };
+
+    setCharacters((prevCharacters) => [...prevCharacters, newCharacter]);
+    console.log("character!", characters);
+  };
+
+  const handleSave = () => {};
+
   return (
     <div className="App">
       <header className="App-header">
@@ -12,10 +38,20 @@ function App() {
       </header>
       <section className="App-section">
         <div>
-          Value:
-          {num}
-          <button>+</button>
-          <button>-</button>
+          <div
+            style={{
+              display: "flex",
+              justifyContent: "center",
+              marginTop: 100,
+            }}
+          >
+            <button style={{ margin: 20 }} onClick={addCharacter}>
+              Add New Character
+            </button>
+            <button style={{ margin: 20 }} onClick={handleSave}>
+              Save Character
+            </button>
+          </div>
         </div>
       </section>
     </div>
