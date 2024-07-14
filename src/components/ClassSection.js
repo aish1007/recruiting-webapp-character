e23d4629d0e4c91059cd36e3f58e54bebcf4f9e7
Aishwarya Narayanan <aishwarya.narayanan025@gmail.com>
2024-07-14 13:20:37 -0700

    Add minimum requirements for classes

--- src/components/ClassSection.js
+++ src/components/ClassSection.js
@@ -1,7 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
+import { CLASS_LIST } from "../consts";
 
 const ClassSection = ({ attributes }) => {
-  return <div>Classes</div>;
+  const [selectedClass, setSelectedClass] = useState(null);
+
+  const isClassAvailable = (className) => {
+    return Object.entries(CLASS_LIST[className]).every(
+      ([attr, minValue]) => attributes[attr] >= minValue
+    );
+  };
+
+  return (
+    <div>
+      <h2>Classes</h2>
+      {Object.keys(CLASS_LIST).map((className) => (
+        <div
+          key={className}
+          style={{ color: isClassAvailable(className) ? "green" : "red" }}
+          onClick={() => setSelectedClass(className)}
+        >
+          {className}
+        </div>
+      ))}
+      {selectedClass && (
+        <div>
+          <h3>{selectedClass} Requirements:</h3>
+          {Object.entries(CLASS_LIST[selectedClass]).map(([attr, value]) => (
+            <div key={attr}>
+              {attr}: {value}
+            </div>
+          ))}
+        </div>
+      )}
+    </div>
+  );
 };
 
 export default ClassSection;
